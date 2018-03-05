018b48f6b35d06c10993561b6c01b21d2e6c7ea5
acaciarose <acaciarudd@gmail.com>
2018-03-05 15:43:56 +0000

    Initial setup of project with Gradle, source and test directories

---
+++ src/main/java/org/stacspics/CommentAPI/Main.java
@@ -0,0 +1,14 @@
+package org.stacspics.CommentAPI;
+
+
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Gradle is working!");
+    }
+
+
+
+}
+
